fb0887692e7921fef033cac7208cf31e2bc19d5a
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-07-06 22:36:33 +0200

    4-DataAccess katmaninin yatilmasi

--- Backend/DataAccess/Mappings/YetkilerMap.cs
+++ Backend/DataAccess/Mappings/YetkilerMap.cs
@@ -1,10 +1,5 @@
 ﻿using Entities.Entities;
 using FluentNHibernate.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Mappings
 {
